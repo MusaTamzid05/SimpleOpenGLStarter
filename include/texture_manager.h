091d4ce357306949470c8524932779b8fffe7480
MusaTamzid05 <tamzid101@gmail.com>
2022-12-15 04:43:34 +0600

    texture mananger

---
+++ include/texture_manager.h
@@ -0,0 +1,43 @@
+#ifndef TEXTURE_MANAGER_H
+#define TEXTURE_MANAGER_H
+
+#include <string>
+#include <map>
+
+
+namespace Mutiny {
+
+    struct TextureData {
+
+        TextureData(const std::string& path,
+                const std::string& name,
+                int width,
+                int height,
+                int channels,
+                int id
+                );
+
+        std::string name;
+        std::string path;
+        int width;
+        int height;
+        int channels;
+        unsigned int id;
+
+    };
+    class TextureManager {
+
+        public:
+            bool add(const std::string& path, const std::string& name);
+            std::map<std::string, TextureData> textures;
+            static TextureManager* get_instance();
+
+
+        private:
+            TextureManager() {}
+            static TextureManager* instance;
+
+    };
+}
+
+#endif
--- src/demos/simple_scene.cpp
+++ src/demos/simple_scene.cpp
@@ -1,9 +1,11 @@
 #include "demos/simple_scene.h"
 #include "scene.h"
 #include "cube.h"
+#include "texture_manager.h"
 
 namespace Demo {
     SimpleScene::SimpleScene(const std::string& title, int width, int height):Scene(title, width, height){
+        Mutiny::TextureManager::get_instance()->add("../textures/wall.jpg", "wall");
             game_obejcts.push_back(new Mutiny::Cube());
     }
 }
